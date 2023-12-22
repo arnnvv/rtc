afce05a934c99e52e2b4a95b956bea0b6e1d2bf3
arnnvv <arnav.900@protonmail.com>
2023-12-22 20:51:13 +0530

    chkPt

---
+++ src/UserManager.ts
@@ -0,0 +1,33 @@
+interface User {
+  name: string;
+  id: string;
+}
+
+interface Room {
+  users: User[];
+}
+
+export class UserManager {
+  private users: Map<string, Room>;
+  constructor() {
+    this.users = new Map<string, Room>();
+  }
+  addUser(name: string, userId: string, roomId: string, socket: WebSocket) {
+    if(!this.users.get(roomId)){
+      this.users.set(roomId, {
+        users: [];
+      })
+    }
+    this.users.get(roomId)?.users.push({
+      id: userId,
+       name
+    })
+  }
+  removeUser(roomId: string, userId: string) {
+        console.log("removed user");
+        const users = this.rooms.get(roomId)?.users;
+        if (users) {
+            users.filter(({id}) => id !== userId);
+        }
+    }
+}
