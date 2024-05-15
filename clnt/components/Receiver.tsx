09857e546af959a62e53ee0602738d2516843de0
arnnvv <arnav.900@protonmail.com>
2024-05-15 23:58:40 +0530

    chkPt

--- clnt/components/Receiver.tsx
+++ clnt/components/Receiver.tsx
@@ -1,3 +1,12 @@
+import { useEffect, useState } from "react";
+
 export const Receiver = () => {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080");
+    socket.onopen = () => {
+      socket.send(JSON.stringify({ type: "sender" }));
+    };
+  }, []);
   return <div>receiver</div>;
 };
--- clnt/components/Sender.tsx
+++ clnt/components/Sender.tsx
@@ -1,3 +1,26 @@
+import { useEffect, useState } from "react";
+
 export const Sender = () => {
-  return <div>Sender</div>;
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080");
+    socket.onopen = () => {
+      socket.send(JSON.stringify({ type: "sender" }));
+    };
+  }, []);
+  return (
+    <div>
+      Sender
+      <button
+        onClick={async () => {
+          //craete an offer
+          const ps = new RTCPeerConnection();
+          const offer = await ps.createOffer();
+          await ps.setLocalDescription(offer);
+        }}
+      >
+        Send Video
+      </button>
+    </div>
+  );
 };
